c7c2f598809220034bc571fe22c2029dd722b175
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-09-17 08:45:38 +0700

    add basic logic for authenticate

---
+++ AuthenticationService.Model/Entities/User/User.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace AuthenticationService.Model.Entities.User;
+
+/// <summary>
+/// System User
+/// </summary>
+public class User : ModelMongoDB
+{
+    /// <summary>
+    /// Display Name
+    /// </summary>
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Email
+    /// </summary>
+    [BsonElement("email")]
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Username
+    /// </summary>
+    [BsonElement("password")]
+    public string Password { get; set; }
+
+    /// <summary>
+    /// Avatar
+    /// </summary>
+    [BsonElement("avatar")]
+    public string Avatar { get; set; } = "";
+
+    /// <summary>
+    /// Role Id
+    /// </summary>
+    [BsonElement("roleIds")]
+    public IList<string> RoleIds { get; set; }
+
+    /// <summary>
+    /// Số điện thoại
+    /// </summary>
+    [BsonElement("phoneNumber")]
+    public string PhoneNumber { get; set; }
+
+    /// <summary>
+    /// Đã kích hoạt hay chưa
+    /// </summary>
+    [BsonElement("isActive")]
+    public bool IsActive { get; set; }
+
+}
---
+++ AuthenticationService/Controllers/AuthenController.cs
@@ -0,0 +1,136 @@
+﻿using AuthenticationService.Middleware;
+using AuthenticationService.Service;
+using AuthenticationService.ViewModel.Dtos;
+using AuthenticationService.ViewModel.Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthenticationService.Controllers;
+
+public class AuthenController : Controller
+{
+    public IUserService UserService { get; set; }
+
+    public IJwtService JwtService { get; set; }
+
+    public AuthenController(IUserService userService, IJwtService jwtService)
+    {
+        UserService = userService;
+        JwtService = jwtService;
+    }
+
+    [HttpPost]
+    [Route("SignUp")]
+    public async Task<ActionResult<UserViewDto>> SignUpAsync([FromBody] CreateUserDto user)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var rs = await UserService.CreateUserAsync(user);
+            if (rs == null)
+            {
+                throw new Exception("User existed");
+            }
+            var refreshToken = JwtService.GenerateRefreshToken(rs.Id);
+            var accessToken = JwtService.GenerateSecurityToken(rs.Id, rs.Roles);
+            SetTokenCookie(refreshToken, accessToken);
+            return Ok(rs);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpPost]
+    [Route("SignIn")]
+    public async Task<ActionResult<UserViewDto>> SignInAsync([FromBody] LoginDto user)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            var rs = await UserService.SignIn(user);
+            if (rs == null)
+            {
+                throw new Exception("Wrong user name or password");
+            }
+            var refreshToken = JwtService.GenerateRefreshToken(rs.Id);
+            var accessToken = JwtService.GenerateSecurityToken(rs.Id, rs.Roles);
+            SetTokenCookie(refreshToken, accessToken);
+            return Ok(rs);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            return BadRequest(e.Message);
+        }
+    }
+
+
+    [HttpGet]
+    [Authorize("User")]
+    [Route("GetUserInfo")]
+    public async Task<ActionResult<UserViewDto>> GetUserAsync()
+    {
+        try
+        {
+            var id = GetId();
+            var rs = await UserService.GetUserByIdAsync(id);
+            return Ok(rs);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("GetAccessToken")]
+    public async Task<ActionResult> GetAccessTokenAsync([Required] string token)
+    {
+        try
+        {
+            if (!JwtService.ValidRefreshToken(token))
+            {
+                return BadRequest("Token expired");
+            }
+            var id = GetId(token);
+            var rs = await UserService.GetUserByIdAsync(id);
+            var accessToken = JwtService.GenerateSecurityToken(id, rs.Roles);
+            SetTokenCookie(token, accessToken);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    private void SetTokenCookie(string refreshToken, string accessToken)
+    {
+        var cookieOptions = new CookieOptions
+        {
+            HttpOnly = true,
+            Expires = DateTime.UtcNow.AddMonths(5)
+        };
+        Response.Cookies.Append("RefreshToken", refreshToken, cookieOptions);
+        Response.Cookies.Append("AccessToken", accessToken, cookieOptions);
+    }
+
+
+    public string GetId()
+    {
+        String token = HttpContext.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last()!;
+        return JwtService.GetId(token);
+    }
+    public string GetId(string token)
+    {
+        return JwtService.GetId(token);
+    }
+}
