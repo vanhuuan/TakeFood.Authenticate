22bcd6c3da18b3eace52e9a073b80891e14696e7
Văn Hữu An <58378623+vanhuuan89@users.noreply.github.com>
2022-11-21 18:53:43 +0700

    fix bug

--- AuthenticationService.ViewModel/Dtos/User/AddAddressDto.cs
+++ AuthenticationService.ViewModel/Dtos/User/AddAddressDto.cs
@@ -36,3 +36,43 @@ public class AddAddressDto
     [Required]
     public double Lng { get; set; }
 }
+
+public class AddressDetailDto
+{
+    /// <summary>
+    /// Addtional information
+    /// </summary>
+    /*[JsonPropertyName("information")]
+    [Required]*/
+    public string AddressID { get; set; }
+    /// <summary>
+    /// Addtional information
+    /// </summary>
+    /*[JsonPropertyName("information")]
+    [Required]*/
+    public string Information { get; set; }
+    /// <summary>
+    /// Address of place
+    /// </summary>
+    [JsonPropertyName("address")]
+    [Required]
+    public string Address { get; set; }
+    /// <summary>
+    /// Address Type
+    /// </summary>
+    [JsonPropertyName("addressType")]
+    [Required]
+    public string AddressType { get; set; }
+    /// <summary>
+    /// Latitude
+    /// </summary>
+    [JsonPropertyName("lat")]
+    [Required]
+    public double Lat { get; set; }
+    /// <summary>
+    /// Longitude
+    /// </summary>
+    [JsonPropertyName("lng")]
+    [Required]
+    public double Lng { get; set; }
+}
